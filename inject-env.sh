ffe2369deae6a2031f22cdd9270d49d806544ec2
Sebastian Krepela <sebous@gmail.com>
2024-03-20 20:04:06 +0100

    feat: working action v1

--- inject-env.sh
+++ inject-env.sh
@@ -25,23 +25,21 @@ if [ -z "$input" ] || [ -z "$path" ] || [ -z "$env" ]; then
 fi
 
 err_message='
-Invalid input, expected input to start with |-
-Example:
-
+Invalid input, expected input formatted like below:
 env_vars: |-
   ONE=1
   TWO=2
 '
 
-# trim leading whitespaces and validate that input starts with a |-
+# trim leading whitespaces
 input=$(sed -e 's/^[[:space:]]*//' <<<"$input")
-[[ $input == "|-"* ]] || {
-	echo $err_message
-	exit 1
-}
 
 json=$(jq -nR '[inputs | select(test("^[^#][^|-]")) | split("=") | {(.[0]): .[1]}] | add' <<<"$input")
 
 # insert json object into eas.json
 jq --arg env "$env" --argjson json "$json" '.build[$env].env = $json' $path >temp.json
+
 mv temp.json $path
+
+keys=$(jq -nR '[inputs | select(test("^[^#][^|-]")) | split("=") | .[0]]' <<<"$input")
+echo echo "Environment variables loaded into eas.json file: $keys"
